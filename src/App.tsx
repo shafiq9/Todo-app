fe139102c27a0ab19cf8bf71bf649aab5b2a30b6
Muhammad Shafiq ur Rehman <shafikrehman@gmail.com>
2025-01-11 02:11:26 +0500

    create form inside App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,22 @@ export default function App() {
         <button className="btn">Add</button>
       </form>
       <h1 className="header">Todo list</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
     </>
   );
 
