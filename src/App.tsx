c3b29309a9ddb012e24406660f2bd77548ec834b
Muhammad Shafiq ur Rehman <shafikrehman@gmail.com>
2025-01-11 02:04:05 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+
+export default function App() {
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo list</h1>
+    </>
+  );
+
+}
